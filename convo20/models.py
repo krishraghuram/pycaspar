6a6101614ab4fc8493a96b52806e785f7a72a209
Anmol Deep <adsk2050@users.noreply.github.com>
2018-06-19 16:41:39 +0530

    created models for VIP and medal

--- convo20/models.py
+++ convo20/models.py
@@ -3,19 +3,56 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-	orderno = models.IntegerField(unique=True)
-	rollno = models.IntegerField(unique=True)
-	name = models.CharField(max_length=100)
+	orderno   = models.IntegerField(unique=True)
+	rollno    = models.IntegerField(unique=True)
+	name      = models.CharField(max_length=100)
 	programme = models.CharField(max_length=50)
-	branch = models.CharField(max_length=50)
+	branch    = models.CharField(max_length=50)
 
 	class Meta:
-		verbose_name="Student"
-		verbose_name_plural="Students"
+		verbose_name        = "Student"
+		verbose_name_plural = "Students"
 
 	def pretty_name(self):
 		if self.name=="" or self.name is None:
 			return self.rollno
+		else: 
+			return self.name
+	pretty_name.short_description = "Name"
+
+
+class VIP(models.Model):
+	orderno     = models.IntegerField(unique=True)
+	name        = models.CharField(max_length=100)
+	designation = models.CharField(max_length=100)
+	
+	class Meta:
+		verbose_name="VIP"
+		verbose_name_plural="VIPs"
+
+	def pretty_name(self):
+		if self.name=="" or self.name is None:
+			return self.designation
+		else: 
+			return self.name
+	pretty_name.short_description = "Name"
+
+
+class Medal(models.Model):
+	orderno   = models.IntegerField(unique=True)
+	rollno    = models.IntegerField(unique=True)
+	medal     = models.CharField(max_length=100)
+	name      = models.CharField(max_length=100)
+	programme = models.CharField(max_length=50)
+	branch    = models.CharField(max_length=50)
+
+	class Meta:
+		verbose_name="Medal Winner"
+		verbose_name_plural="Medal Winners"
+
+	def pretty_name(self):
+		if self.name=="" or self.name is None:
+			return self.medal
 		else: 
 			return self.name
 	pretty_name.short_description = "Name"
